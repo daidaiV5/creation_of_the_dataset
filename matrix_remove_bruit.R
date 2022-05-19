b44b8877fbda2023379d2f0664662ce955107cd8
Yuping DAI <daiyuping192@gmail.com>
2022-05-19 15:41:46 +0200

    test

--- matrix_remove_bruit.R
+++ matrix_remove_bruit.R
@@ -2,19 +2,20 @@
 library("Rsubread")
 library('ggplot2')
 library("DESeq2")
+library("limma")
 
 ### remove differents type RNA keep only mRNA
 
 
 #### load raw data
-matrice_count=read.csv("/Users/daiyuping/Desktop/gitstage/dataselection/creation_of_the_dataset/matrice_count.csv",row.names = 1)
+matrice_count=read.csv("/home/storage_1/yuping/git_selection/matrice_count_hypo.csv",row.names = 1)
 
-file_mat='/Users/daiyuping/Desktop/gitstage/dataselection/creation_of_the_dataset/matrice_count.csv'
-matrice_mRNA=read.csv(file=file_mat,header=TRUE, sep=",", row.names = 1)
+file_mat='/home/storage_1/yuping/git_selection/matrice_count_hypo.csv'
+matrice_count=read.csv(file=file_mat,header=TRUE, sep=",", row.names = 1)
 
 #### read annotation of gene
 
-annotation_grf=read.csv("/Users/daiyuping/Desktop/gitstage/dataselection/creation_of_the_dataset/annotation_grf.csv",row.names = 1)
+annotation_grf=read.csv("/home/storage_1/yuping/git_selection/annotation_grf.csv",row.names = 1)
 
 
 #### remove differents type RNA keep only mRNA
@@ -25,7 +26,7 @@ annotation_autre=setdiff(annotation_total,annotation_mRNA)
 matrice_non_mRNA=matrice_count[annotation_autre,]
 
 ### read sample annotation
-annotation_sample=read.csv("/Users/daiyuping/Desktop/gitstage/dataselection/creation_of_the_dataset/liver_final.csv", sep = ";",row.names = 'SRR',header = TRUE)
+annotation_sample=read.csv("/home/storage_1/yuping/git_selection/hypocampus_final.csv", sep = ";",row.names = 'SRR',header = TRUE)
 annotation_sample=annotation_sample[c('Sex','age_class','series_id')]
 
 cts <- as.matrix(matrice_mRNA)
@@ -33,21 +34,21 @@ cts= cts[,order(names(matrice_mRNA))]
 
 coldata=annotation_sample[order(row.names(annotation_sample)),]
 
+
 all(rownames(coldata) == colnames(cts))
 
-
+  
 dds <- DESeqDataSetFromMatrix(countData = cts,
                               colData = coldata,
-                              design = ~ series_id)
+                              design = ~ Sex+age_class)
 
 
 dds <- DESeq(dds)
 resMF <- results(dds)
 
-keep <- rowSums(counts(dds)) >= 20							# Parameter: keep only counts >= 20, if keep only counts >=10,removeBatchEffect will return value negetif ***
+keep <- rowSums(counts(dds)) >= 20							# Parameter: keep only counts >= 20, if keep only counts >=10,removeBatchEffect will return value negetif
 dds <- dds[keep,]
 vsd <- vst(dds, blind=FALSE)
-
 ### batch effect
 
 
@@ -59,24 +60,25 @@ which(assay(vsd)==min(assay(vsd)),arr.ind=TRUE) ## make sure all value is positi
 
 matrix_final=assay(vsd)
 
-write.csv(matrix_final,"/Users/daiyuping/Desktop/gitstage/dataselection/creation_of_the_dataset/matrice_final_liver_remove.csv")
+write.csv(matrix_final,"/home/storage_1/yuping/git_selection/matrice_final_Hypo_remove.csv")
+write.csv(coldata,"/home/storage_1/yuping/git_selection/annotation_final_Hypo_remove.csv")
 
-### plot with series_id(GEO)
-# pcaData <- plotPCA(vsd, intgroup=c("series_id"), returnData=TRUE)
-# percentVar <- round(100 * attr(pcaData, "percentVar"))
-# ggplot(pcaData, aes(PC1, PC2, color=series_id,label = rownames(coldata))) +
-#   geom_point(size=3) +
-#   xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-#   ylab(paste0("PC2: ",percentVar[2],"% variance")) +
-#   coord_fixed()+ggtitle('Principal component plot of the sample-to-sample distances')
-# 
-# 
+  ### plot with series_id(GEO)
+pcaData <- plotPCA(vsd, intgroup=c("series_id"), returnData=TRUE)
+percentVar <- round(100 * attr(pcaData, "percentVar"))
+ggplot(pcaData, aes(PC1, PC2, color=series_id,label = rownames(coldata))) +
+  geom_point(size=3) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+  coord_fixed()+ggtitle('Principal component plot of the sample-to-sample distances')
+
+
 
 ### plot with age_class and sex
 library("ggplot2")
 pcaData <- plotPCA(vsd, intgroup=c("age_class","Sex"), returnData=TRUE)
 percentVar <- round(100 * attr(pcaData, "percentVar"))
-ggplot(pcaData, aes(PC2, PC1, color=age_class, shape=Sex,label = rownames(coldata))) +
+ggplot(pcaData, aes(PC1, PC2, color=age_class, shape=Sex,label = rownames(coldata))) +
   geom_point(size=3) +
   xlab(paste0("PC1: ",percentVar[1],"% variance")) +
   ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
@@ -85,11 +87,22 @@ ggplot(pcaData, aes(PC2, PC1, color=age_class, shape=Sex,label = rownames(coldat
 
 
 ###seperation of the matrix for each class of age
+library("do")
+
 class_age=unique(coldata$age_class)
 sex=unique(coldata$Sex)
 
+for(j in sex){
+  x=strsplit(j,": ")[[1]]
+  x=x[2]
+  print(x)
+}
+
+
+dir.create('/home/storage_1/yuping/GCN/dataset_test/hypo_mouse/matrice_hypo')
+dir.create('/home/storage_1/yuping/GCN/dataset_test/hypo_mouse/annotation_hypo')
 for (i in class_age) {
-  for( j in sex){
+for(j in sex){
     print(i)
     print(j)
     row_name=c(row.names(annotation_sample)[which(annotation_sample$Sex==j)])
@@ -97,21 +110,22 @@ for (i in class_age) {
     coldata=annotation_sample[row_name,]
     cts_sex_class=cts[,c(row.names(coldata)[which(coldata$age_class==i)])]
     coldata_sex_class=coldata[c(row.names(coldata)[which(coldata$age_class==i)]),]
-    name_of_cts=paste('/Users/daiyuping/Desktop/gitstage/dataselection/creation_of_the_dataset/matrice_liver2/liver_',i,'_',j,'.csv',sep="")
-    name_of_coldata=paste('/Users/daiyuping/Desktop/gitstage/dataselection/creation_of_the_dataset/annotation_liver2/annotation_liver_',i,'_',j,'.csv',sep="")
+    x=strsplit(j,": ")[[1]]
+    j=x[2]
+    name_of_cts=paste('/home/storage_1/yuping/GCN/dataset_test/hypo_mouse/matrice_hypo/hypo_',i,'_',j,'.csv',sep="")
+    name_of_coldata=paste('/home/storage_1/yuping/GCN/dataset_test/hypo_mouse/annotation_hypo/annotation_hypo_',i,'_',j,'.csv',sep="")
     print(name_of_cts)
     print(name_of_coldata)
-    write.csv(cts_sex_class,name_of_cts)
-    write.csv(coldata_sex_class,name_of_coldata)
+    write.csv2(cts_sex_class,name_of_cts,sep=',')
+    write.csv2(coldata_sex_class,name_of_coldata,sep=',')
     }
 }
 
 
 
-
 ### annotation of ontology
 ###BiocManager::install('org.Mm.eg.db',force = TRUE)
-###BiocManager::install("clusterProfiler")
+###BiocManager::install("clusterProfiler")  
 
 library('org.Mm.eg.db')
 library("clusterProfiler")
